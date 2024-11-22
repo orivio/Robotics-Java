852e20adece953bd5cc3895a1237e22c57276867
Russlan <russlanhossain@gmail.com>
2024-11-21 18:39:58 -0800

    Finished the Practice Problems, finally!

--- java101/Main.java
+++ java101/Main.java
@@ -1,11 +1,6 @@
-package java101;
-
 public class Main {
 public static void main(String[] args) {
-    int[] arr = {1, 2, 3, 4, 5, 3, 2};
-    int n = 4;
+    int n = 8;
 
-    int result = countOccurrences(arr, n);
-    System.out.println(result);
-    }
-}
+    System.out.println(PracticeProblems.fib(n));
+    }}
--- java101/PracticeProblems.java
+++ java101/PracticeProblems.java
@@ -8,7 +8,39 @@ public class PracticeProblems {
                 sum++;
             }
             return sum;
+            // worked successfully
     }
+    //practice 2
+    static int[] reverseArray(int[] arr) {
+        int[] reverseArr = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            reverseArr[i] = arr[arr.length - 1 - i];
+    
+        }
+        return reverseArr;
+        // worked successfully
+    }
+    //practice 3
+    static double sumGrid(double[][] grid) {
+        double sum = 0;
+        for(double[] row : grid) {
+            for(double value : row){
+                sum += value;
 
-
+            }
+        }
+        return sum;
+        //worked successfully
+    }
+    //practice 4 FINALLY
+    static int fib(int n){
+        if (n <= 0) {
+            return 0;
+        } else if (n ==1) {
+            return 1;
+        } else{
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+    //WORKED SUCCESSFULLY!!!!!!!!!!
 }
